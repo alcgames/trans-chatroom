b34b5b4966c7f7769c6f0d24ff9acd4bae84d844
alcgames <tylerdavidmckeown@icloud.com>
2026-01-13 00:18:32 +0000

    Create chat.js

---
+++ chat.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDOn6h4zU98aN78hO8TlCmY9a0I8vcNAwg",
+  authDomain: "trans-chatroom.firebaseapp.com",
+  projectId: "trans-chatroom",
+  storageBucket: "trans-chatroom.firebasestorage.app",
+  messagingSenderId: "730363722376",
+  appId: "1:730363722376:web:7437bcb1d304ec73d77d3d",
+  measurementId: "G-4BLZ116JTJ"
+};
